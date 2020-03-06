6fcce26a445b6cf55d8922fea07819accc1a7299
MrCasades <casades@list.ru>
2020-03-06 22:20:05 +0300

    upd footer

--- footer.inc.php
+++ footer.inc.php
@@ -4,7 +4,7 @@
 	<p>Copyright © 2019 MrCasades. All rights reserved.</p>
 </div>
 	<script src="/js/jquery-1.min.js"></script>
-	<script src="./script.js"></script>
+	<script src="/script.js"></script>
 	<script src="/js/bootstrap-markdown.js"></script>
 	<script src="/js/bootstrap.min.js"></script>
 
